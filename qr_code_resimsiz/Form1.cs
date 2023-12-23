3abeb4cf4ab4bb5c80ac0664805977081e4fd885
Ayşe Şensoy <51344094+aysesensoy@users.noreply.github.com>
2023-12-23 18:54:51 +0300

    Add project files.

---
+++ qr_code_resimsiz/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QRCoder;
+
+namespace qr_code_resimsiz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string metin_Code = textBox1.Text;
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(metin_Code, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            pictureBox1.Image = qrCodeImage;
+        }
+    }
+}
+
